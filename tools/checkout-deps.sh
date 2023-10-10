40e352b4b40d79046c156d2d1921dc008d231590
Dominik Adrian Grzywak <starterx4@gmail.com>
2023-10-10 16:50:13 +0200

    Update checkout-deps.sh

--- tools/checkout-deps.sh
+++ tools/checkout-deps.sh
@@ -126,7 +126,7 @@ if [ -z ${sdks+x} ]; then
 
   if [ $ismac -eq 0 ]; then
     # Add these SDKs for Windows or Linux
-    sdks+=( orangebox blade episode1 bms pvkii )
+    sdks+=( orangebox blade episode1 bms pvkii cs2 )
 
     # Add more SDKs for Windows only
     if [ $iswin -eq 1 ]; then
